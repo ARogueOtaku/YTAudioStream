98afd478a02a3901dce0cc71bfa7ea46161e5641
AntiPrim3 <amit.rockinad.das@gmail.com>
2021-05-18 00:21:54 +0530

    Updated with Exception Handling

--- server.js
+++ server.js
@@ -5,19 +5,28 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  res.status(400).send("Video Id Required! BAD REQUEST!");
+});
+
 app.get("/:url", async (req, res) => {
   const url = req.params.url;
-  if (!url) res.status(400).send("Video Id Required! BAD REQUEST!");
   const quality = req.query.quality || "highest";
   res.setHeader("content-type", "audio/webm");
   res.setHeader("Access-Control-Expose-Headers", "title");
   const info = await ytdl.getInfo(url);
   res.setHeader("title", encodeURI(info.videoDetails.title));
   console.log(req.origin);
-  ytdl("http://www.youtube.com/watch?v=" + url, {
-    quality: quality,
-    filter: "audioonly",
-  }).pipe(res);
+  try {
+    ytdl("http://www.youtube.com/watch?v=" + url, {
+      quality: quality,
+      filter: "audioonly",
+    }).pipe(res);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    res.send();
+  }
 });
 
 const listener = app.listen(process.env.PORT, () => {
